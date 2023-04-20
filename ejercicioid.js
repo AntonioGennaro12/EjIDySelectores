b5aebfc9853cb47f0e229ca50a86c98418266685
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-20 11:31:04 -0300

    Ej ID c/Selectores v3

--- ejercicioid.js
+++ ejercicioid.js
@@ -1,14 +1,16 @@
 const Sumario   = document.querySelector("#mi-sum");
 const Imagen    = document.querySelector("#mi-imagen");
-const Parrafo   = document.querySelector("#mi-parrafo");
+const Parrafo1  = document.querySelector("#mi-parrafo1");
+const Parrafo2  = document.querySelector("#mi-parrafo2");
 const Link      = document.querySelector("#mi-link");
 
-Sumario.textContent = "++++ Muestro una pantalla con Datos ++++";
-Imagen.width        = '300';
-Imagen.height       = '200';
-Imagen.src          = 'https://upload.wikimedia.org/wikipedia/commons/2/23/Midnight_Commander_%282007%29_de.png';
-Parrafo.textContent = "Uso este Link para ver el ejercicio ID & Selectores";
-Link.textContent    = "Enlace_al_Ejercicio_Id_y_Selectores.pdf";
+Sumario.textContent  = "++++ Ejercio ID y Selectores ++++";
+Parrafo1.textContent = "Incluyo una Pantalla con Datos (tamaño a justado)";
+Imagen.width         = '320';
+Imagen.height        = '240';
+Imagen.src           = 'https://upload.wikimedia.org/wikipedia/commons/2/23/Midnight_Commander_%282007%29_de.png';
+Parrafo2.textContent = "Uso este Link para ver el ejercicio ID & Selectores";
+Link.textContent     = "Enlace_al_Ejercicio_Id_y_Selectores.pdf";
 // Tambien puedo utilizar "InnerHTML"
 //Link.innerHTML      = "Enlace_al_Ejercicio_Id_y_Selectores.pdf";
 Link.href           = 'https://docs.google.com/presentation/d/1lAR2EewB7pa0dDjJaagQYVHA0WDRN_pjQ6rG1N-7Q9I/edit?usp=sharing';
