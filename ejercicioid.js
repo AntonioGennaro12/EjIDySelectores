ee306fd13e3c3a631f3497f1ed8a04c838f24e88
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-20 01:18:13 -0300

    Ej ID c/Selectores v1

---
+++ ejercicioid.js
@@ -0,0 +1,14 @@
+const Sumario   = document.querySelector("#mi-sum");
+const Imagen    = document.querySelector("#mi-imagen");
+const Parrafo   = document.querySelector("#mi-parrafo");
+const Link      = document.querySelector("#mi-link");
+
+Sumario.textContent = "MUESTRA UNA PANTALLA CON DATOS";
+Imagen.width        = '300';
+Imagen.height       = '200';
+Imagen.src          = 'https://upload.wikimedia.org/wikipedia/commons/2/23/Midnight_Commander_%282007%29_de.png';
+Parrafo.textContent = "Ahora uso este Link para ver el ejercicio ID & Selectores";
+Link.href           = 'https://docs.google.com/presentation/d/1lAR2EewB7pa0dDjJaagQYVHA0WDRN_pjQ6rG1N-7Q9I/edit?usp=sharing';
+
+
+
